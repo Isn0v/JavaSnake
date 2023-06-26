b262fa419d4e575ed23c6e301b01e5b496fb8088
Isn0v <isnovikov0@gmail.com>
2023-06-26 22:17:14 +0700

    SnakeGame continue

---
+++ src/main/java/org/example/MyServer.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.io.IOException;
+
+public class MyServer extends  Server{
+    public MyServer(int port) {
+        super(port);
+    }
+
+    @Override
+    public void OnMessage(ClientManager clientManager, String message) throws IOException {
+        sendAll(clientManager + ": " + message);
+    }
+
+
+}
